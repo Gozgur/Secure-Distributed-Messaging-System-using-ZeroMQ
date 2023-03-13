3ef4436534f2e9a2a0cff604f44c2fb81019266b
gokselozgur5 <gokselozgur5@gmail.com>
2023-03-13 20:54:39 +0300

    ADD: class object

--- server.py
+++ server.py
@@ -1,2 +1,14 @@
 import zmq.auth
 from zmq.auth.thread import ThreadAuthenticator
+
+class SecureMessagingSystem:
+    def __init__(self):
+        self.context = zmq.Context.instance()
+        self.socket = self.context.socket(zmq.ROUTER)
+        self.socket.bind("tcp://*:5555")
+        
+        self.authenticator = ThreadAuthenticator(self.context)
+        self.authenticator.start()
+        self.authenticator.allow("127.0.0.1")
+        self.authenticator.configure_curve(domain="*", location='clients')
+        self.authenticator.authenticator = self.my_zap_handler
