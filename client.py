c8f0ad62da8fbfa09488aeca0c34f1a161f8c74c
gokselozgur5 <gokselozgur5@gmail.com>
2023-03-13 20:48:38 +0300

    ADD: client send_message function

--- client.py
+++ client.py
@@ -5,3 +5,8 @@ class ZmqClient:
         self.context = zmq.Context.instance()
         self.socket = self.context.socket(zmq.DEALER)
         self.socket.connect(f"tcp://{host}:{port}")
+
+    def send_message(self, message):
+        self.socket.send(message)
+        response = self.socket.recv()
+        print("Received response: %s" % response)
