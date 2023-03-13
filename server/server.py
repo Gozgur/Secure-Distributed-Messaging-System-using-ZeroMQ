f93a43b514962c7c8effcc539332e9126a736ba8
gokselozgur5 <gokselozgur5@gmail.com>
2023-03-13 23:55:46 +0300

    ADD: server send and receive

--- server/server.py
+++ server/server.py
@@ -2,12 +2,13 @@ import zmq.auth
 from zmq.auth.thread import ThreadAuthenticator
 from pathlib import Path
 from pathlib import Path
+from zmq.auth.asyncio import AsyncioAuthenticator
 
 # Get the current working directory
 current_dir = Path.cwd()
 
 # Navigate to the clients directory
-clients_dir = current_dir / "server" / "clients"
+clients_dir = current_dir / "server" / "clients" / "."
 
 # Navigate to the cert directory
 cert_dir = current_dir / "server" / "cert"
@@ -20,19 +21,29 @@ class SecureMessagingSystem:
         self.context = zmq.Context.instance()
         self.socket = self.context.socket(zmq.ROUTER)
         self.socket.bind("tcp://*:5555")
-        
+        self.server_public, self.server_secret = zmq.auth.load_certificate(str(key_file_path))
+        self.curve_server = True
+        self.curve_publickey = self.server_public
+        self.curve_secretkey = self.server_secret
         self.authenticator = ThreadAuthenticator(self.context)
+        
         self.authenticator.start()
+
         self.authenticator.allow("127.0.0.1")
         self.authenticator.configure_curve(domain="*", location=str(clients_dir))
-        self.curve_secretkey, self.curve_publickey = zmq.auth.load_certificate(str(key_file_path))
         self.authenticator.curve_server = True
+
 
     def start(self):
         while True:
+            
             message = self.socket.recv_multipart()
             identity, payload = message[0], message[1:]
             print(f"Received request from {identity}: {payload}")
             response = [identity, b"Hello, client!"]
             self.socket.send_multipart(response)
 
+
+if __name__ == '__main__':
+    messaging_system = SecureMessagingSystem()
+    messaging_system.start()
