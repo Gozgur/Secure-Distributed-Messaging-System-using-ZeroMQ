0de737159e3804b931074271be223647542f8723
gokselozgur5 <gokselozgur5@gmail.com>
2023-03-13 20:47:57 +0300

    ADD: client class

--- client.py
+++ client.py
@@ -1,1 +1,7 @@
 import zmq
+
+class ZmqClient:
+    def __init__(self, host, port):
+        self.context = zmq.Context.instance()
+        self.socket = self.context.socket(zmq.DEALER)
+        self.socket.connect(f"tcp://{host}:{port}")
